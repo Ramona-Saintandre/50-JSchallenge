a9e5613110b16c45c386587af1079743ad57480c
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-18 22:13:04 -0500

    timer is working

--- JS-Countdown/main.js
+++ JS-Countdown/main.js
@@ -2,20 +2,27 @@
 
 
 var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
-var x = setInterval(function() {  
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
+var x = setInterval(function () {
+    var now = new Date().getTime();
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var distance = countDownDate - now;
 
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+
+    //   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    //   + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+
+    }
 }, 1000);
