9ea5d6c7c24e1f1a60ac3c4776d542007587104d
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-20 21:11:10 -0500

    cleared the var function, using let from MDN

--- JS-guessinggame/app.js
+++ JS-guessinggame/app.js
@@ -1,4 +1,18 @@
+let ranNum = Math.floor(Math.random() *100)+1;
 
-function generateRanNum(){
-return Math.floor((Math.random()*100)+1);
-}
+const guess = document.querySelector('.guess')
+
+// from thinkful
+
+// This code uses jQuery, a javascript library, to run.
+// You don't need to know how this works, 
+// just that it makes the submit button function.
+// $('.guessingForm').submit(function (event) {
+// event.preventDefault();
+// var guess = $('#js-user-guess').val();
+// console.log(`The guess is ${guess}. The random number is ${randomNumber}.`)
+// $('#js-user-guess').val('');
+// checkGuess(guess);
+// });
+// // document.ready end    
+// });
