88dc6c170ab52b840abb71c24bdea4245dc1db3c
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-24 17:30:59 -0500

    added forEach loop for slide show

--- JS-timeline-slideshow/main.js
+++ JS-timeline-slideshow/main.js
@@ -1,6 +1,19 @@
+let current =1;
+
 const changeSlides = () => {
     const slideList =
     document.querySelectorAll('.slide')
 
 const slides = Array.from(slideList);
+
+slides.forEach(slide =>{
+    if(slide.classList[1].split("-")[1]*1
+     === current){
+        slide.style.cssText = 'visiablity:visible; opacity:1';    
+   
+} else {
+    slide.style.cssText = 'visiablity:visible; opacity:0';
 }
+});
+};
+
