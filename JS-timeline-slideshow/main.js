80d0813dfe7a1dc3aed115aed0e064e2372ce4fc
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-24 21:50:49 -0500

    added arrow visibilty, now going to make it work

--- JS-timeline-slideshow/main.js
+++ JS-timeline-slideshow/main.js
@@ -1,59 +1,71 @@
-let current =1;
+let current = 1;
 
 const changeSlides = () => {
     const slideList =
-    document.querySelectorAll('.slide')
+        document.querySelectorAll('.slide')
 
-const slides = Array.from(slideList);
+    const slides = Array.from(slideList);
 
-slides.forEach(slide =>{
-    if(slide.classList[1].split("-")[1]*1
-     === current){
-        slide.style.cssText = 'visiablity:visible; opacity:1';    
-   
-} else {
-    slide.style.cssText = 'visiablity:visible; opacity:0';
-}
-});
+    slides.forEach(slide => {
+        if (slide.classList[1].split("-")[1] * 1 ===
+            current) {
+            slide.style.cssText = 'visiablity:visible; opacity:1';
+
+        } else {
+            slide.style.cssText = 'visiablity:visible; opacity:0';
+        }
+    });
 };
 
 // making the play button work
-const playPause =() => {
-    if(playPauseBool){
-       interval = setInterval(() =>{
-            current++;
-            changeSlides();
-        },3000);
-      
-    playPauseBool = false;
-}else{
-    clearInterval(interval);
-    playPauseBool =true;
-}
-playPauseBtn();
-};
+const arrowVisibility = () => {
+    const arrows = document.querySelectorAll(".control");
+    Array.from(arrows).forEach(arrow => {
+        if (!playPauseBool) {
+            arrow.classList.remove("arrows.visibility");
 
+        } else {
+            arrow.classList.remove("arrows-visibility");
+        }
+    })
+};
 //  creating the pause button
 
-const playPauseBtn = ()=> {
-    const icon = document.querySelector('.play-pause i');
+const playPauseBtn = () => {
+    const i = document.querySelector(".play-pause i");
 
-    const cls = icon.classList[1]
-    if (cls === 'fa-play'){
-        icon.classList.remove('fa-play');
-        icon.classList.add('fa-pause');
-    }else{
-        icon.classList.remove('fa-pause');
-        icon.classList.add('fa-play');
+    const cList = i.classList[1];
+
+    if (cList === "fa-play") {
+        i.classList.remove("fa-play");
+        i.classList.add("fa-pause");
+    } else {
+        i.classList.remove("fa-pause");
+        i.classList.add("fa-play");
     }
+};
+
+const playPause = () => {
+    if (playPauseBool) {
+        interval = setInterval(() => {
+            current++;
+            changeSlides();
+        }, 3000);
+        playPauseBool = false;
+    } else {
+        clearInterval(interval);
+        playPauseBool = true;
+    }
+    arrowVisibility();
+    playPauseBtn();
 };
 
 
 // make play button work
-document.querySelector('.play-pause')
-.addEventListener('click',()=>{
-    playPause();
-});
+document.querySelector("play-pause")
+    .addEventListener('click', () => {
+        playPause();
+    });
 
 // calling the function
 changeSlides();
